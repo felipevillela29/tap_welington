c11a3a75e125e04b8d7f1360e46b8ce197096bba
Felipe V. Souza <felipevillela.2006@gmail.com>
2024-03-13 15:39:39 -0300

    Exercicios de Vetor

---
+++ vetor/exc1/exc1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace exc1
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            // Definindo o tamanho do vetor
+            int[] numeros = new int[5];
+
+            Console.WriteLine("digite 5 numeros inteiros:");
+
+
+            // vai ler os números do usuário e armazenar no vetor
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"Numero {i + 1}: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // vai inicializar variáveis para armazenar o maior e o menor número
+            int maior = numeros[0];
+            int menor = numeros[0];
+
+            // percorre o vetor para encontrar o maior e o menor número
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                if (numeros[i] > maior)
+                {
+                    maior = numeros[i];
+                }
+                if (numeros[i] < menor)
+                {
+                    menor = numeros[i];
+                }
+            }
+
+            // vai exibir o maior e o menor número
+            Console.WriteLine($"O maior número é: {maior}");
+            Console.WriteLine($"O menor número é: {menor}");
+        }
+    }
+
+}
---
+++ vetor/exc2/exc2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Defini o tamanho do vetor
+        int[] numeros = new int[10];
+
+        Console.WriteLine("Digite 10 números inteiros:");
+
+        // ler os números do usuário e armazenar no vetor
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Solicita o número alvo ao usuário
+        Console.Write("Digite o número alvo para buscar no vetor: ");
+        int numeroAlvo = Convert.ToInt32(Console.ReadLine());
+
+        // verifica se o número alvo está presente no vetor
+        bool numeroEncontrado = false;
+        foreach (int numero in numeros)
+        {
+            if (numero == numeroAlvo)
+            {
+                numeroEncontrado = true;
+                break; // vai sair do loop se o número for encontrado
+            }
+        }
+
+        // Exibir o resultado
+        if (numeroEncontrado)
+        {
+            Console.WriteLine($"O número {numeroAlvo} foi localizado no vetor.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numeroAlvo} não foi localizado no vetor.");
+        }
+    }
+}
